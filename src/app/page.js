2b859e044f10df982bd83fa8bd0fe24b9e863c1a
mario-97 <marioalfredo9@gmail.com>
2025-03-10 16:24:24 -0300

    update

--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,12 @@ export default function Home() {
 
     return (
         <div className="w-screen h-screen flex flex-col">
-            <h1 className="text-xl font-bold p-4 bg-gray-100">
+            <h1 className="text-xl font-bold p-4 bg-black-100">
                 Visualización del Reporte de Power BI
             </h1>
             <div className="flex-grow">
-                <iframe
+              Texto de prueba
+                {/* <iframe
                     src={powerBIUrl}
                     width="100%"
                     height="100%"
@@ -20,7 +21,7 @@ export default function Home() {
                     allowFullScreen
                     title="Reporte Power BI"
                     className="w-full h-full"
-                ></iframe>
+                ></iframe> */}
             </div>
         </div>
     );
