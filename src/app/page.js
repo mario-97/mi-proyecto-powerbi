1ca3346b01d7c16017a53b05d124e343b9518fa3
mario-97 <marioalfredo9@gmail.com>
2025-03-10 16:09:36 -0300

    proyecto-powerbi

---
+++ src/app/page.js
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+
+export default function Home() {
+    // URL del informe de Power BI
+    const powerBIUrl = "https://app.powerbi.com/view?r=eyJrIjoiOTA2NTEwYmMtMmI2MS00NGJhLTg0ZTEtYWFlNzhmODk0NTIxIiwidCI6IjMyYTQ3ZjJkLTZlYjItNDIyNC04YjExLTI1MTk3NTQ1ODFjNSIsImMiOjR9";
+
+    return (
+        <div className="w-screen h-screen flex flex-col">
+            <h1 className="text-xl font-bold p-4 bg-gray-100">
+                Visualización del Reporte de Power BI
+            </h1>
+            <div className="flex-grow">
+                <iframe
+                    src={powerBIUrl}
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    allowFullScreen
+                    title="Reporte Power BI"
+                    className="w-full h-full"
+                ></iframe>
+            </div>
+        </div>
+    );
+}
+
